2f3c458b813e2d42b9013120498c1ae2fcb2df1f
LeverUp-Dev <ododo3@naver.com>
2025-04-26 18:26:35 +0900

    edit 변수와 변수 주소값

--- source.cpp
+++ source.cpp
@@ -1,4 +1,9 @@
-#include <iostream>
+#include <iostream>	//전처리 지시자
+
+/*
+C++에서 함수를 사용하고자 한다면...
+반드시 함수의 원형을 미리 정의하여야 한다
+*/
 
 using namespace std;
 
@@ -7,6 +12,10 @@ int main() {
 	printf("Hello, World!\n");	// C 언어 형식
 
 	cout << "Hello, World!" << endl; // C++ 형식
+
+	int a, b;
+
+	cout << "a = " << &a << ", b = " << &b << endl;
 
 	return 0;
 }
