f6bd4ca6c336c242bd4f74cf889490b8d538b165
LeverUp-Dev <ododo3@naver.com>
2025-04-26 20:23:21 +0900

    edit: 공용체와 열거체

--- source.cpp
+++ source.cpp
@@ -221,6 +221,41 @@ int main() {
 	cout << P[0].name << endl;
 	*/
 
+	//공용체(union)
+	// 서로 다른 데이터형을 한 번에 한 가지만 보관 가능함
+	/*
+	union MyUnion
+	{
+		int intVal;
+		long longVal;
+		float floatVal;
+	};
+
+	MyUnion test;
+	test.intVal = 3;
+	cout << test.intVal << endl;
+	test.intVal = 33;
+	cout << test.intVal << endl;
+	cout << test.longVal << endl;
+	test.floatVal = 3.14;
+	cout << test.intVal << endl;
+	cout << test.longVal << endl;
+	cout << test.floatVal << endl;
+	*/
+	//열거체(enum)
+	// 기호, 상수를 만드는 것에 대한 또 다른 방법
+	/*
+	enum spectrum { red, orange, yellow, green, blue, violet, indigo, ultraviolet };
+
+	spectrum a = blue;
+	cout << a << endl;
+
+	int b;
+	b = blue;
+	b = blue + 3;
+	cout << b << endl;
+	*/
+
 	
 
 	return 0;
